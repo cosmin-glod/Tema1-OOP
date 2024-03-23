53c8bafe94030f7b29d93565d187a08417b0d7b7
Cosmin371 <glod.cosmin@gmail.com>
2024-03-23 22:02:04 +0200

    Salut

---
+++ Game.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Cosmin on 23.03.2024.
+//
+
+#include "Game.h"
---
+++ Game.h
@@ -0,0 +1,16 @@
+//
+// Created by Cosmin on 23.03.2024.
+//
+
+#ifndef OOP_GAME_H
+#define OOP_GAME_H
+
+
+
+class Game {
+
+};
+
+
+
+#endif //OOP_GAME_H
